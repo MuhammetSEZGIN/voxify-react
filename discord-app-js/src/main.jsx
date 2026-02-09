4d42423362c01020bd30a22d8eb8eff142a5588c
Muhammet Sezgin <msezgin@Muhammet-MacBook-Air.local>
2026-02-09 14:31:19 +0300

    Summarize listed file changes

--- discord-app-js/src/main.jsx
+++ discord-app-js/src/main.jsx
@@ -2,14 +2,29 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
-
 import App from "./App";
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </StrictMode>
-);
+
+
+async function enableMocking() {
+  if (import.meta.env.MODE !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+
+  return worker.start({
+    onUnhandledRequest: 'bypass', // Don't warn about unhandled requests
+  });
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById("root")).render(
+    <StrictMode>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </StrictMode>
+  );
+});
